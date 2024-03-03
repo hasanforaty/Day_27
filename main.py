4c575455c6a792d3245642178b268fa18bc2c647
hasan foraty <hasanforaty19@gmail.com>
2024-03-03 10:41:37 +0330

    get layout manager system of Tkinter

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("My First GUI Program")
-window.minsize(width=800, height=600)
-
 
 def button_clicked():
     my_label['text'] = our_input.get()
 
 
+window = tk.Tk()
+window.title("My First GUI Program")
+window.minsize(width=800, height=600)
+window.config(padx=20, pady=20)
+
 # Label
 
 my_label = tk.Label(text='I am a label')
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 button = tk.Button(text='Click Me', command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+button = tk.Button(text='new Button', command=button_clicked)
+button.grid(column=2, row=0)
 
 our_input = tk.Entry()
-our_input.pack()
+our_input.grid(column=3, row=2)
 
 window.mainloop()
